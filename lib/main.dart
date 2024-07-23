b3e565824cc6879545bc86c1a53686d629873ee9
woragis <masteringthecode.woragis+github@gmail.com>
2024-07-23 17:46:03 -0300

    Transform Stateless Widget into Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,101 @@
 import 'package:flutter/material.dart';
 
 // Static Variables
-const String username = "Chun-Li";
-const String email = "chunli@streetfight.com";
+const nameLabel = "NAME";
 
 void main() {
   runApp(const MaterialApp(home: NinjaCard()));
 }
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
   const NinjaCard({Key? key}) : super(key: key);
+
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
+
+  double bodyPadding = 40.0;
+  String avatarPath = "assets/chun-li.jpeg";
+  double dividerHeight = 60.0;
+  double sizedBoxHeight = 10.0;
+  String username = "Chun-Li";
+  String email = "chunli@streetfight.com";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black45,
+        backgroundColor: const Color.fromARGB(255, 15, 15, 15),
         appBar: AppBar(
           title: const Text("Ninja Card ID"),
-          backgroundColor: Colors.black12,
+          backgroundColor: const Color.fromARGB(255, 41, 41, 41),
           centerTitle: true,
           elevation: 0.0,
         ),
-        floatingActionButton: const Text("Hi"),
-        body: const Padding(
-          padding: EdgeInsets.all(40.0),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              ninjaLevel++;
+            });
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(bodyPadding),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               CircleAvatar(
-                backgroundImage: AssetImage("assets/chun-li.jpeg"),
+                backgroundImage: AssetImage(avatarPath),
               ),
               Divider(
-                height: 60.0,
+                height: dividerHeight,
                 color: Colors.grey,
               ),
-              Text(
-                "NAME",
+              const Text(
+                nameLabel,
                 style: TextStyle(
                   color: Colors.grey,
                   letterSpacing: 2.0,
                 ),
               ),
-              SizedBox(height: 10.0),
+              SizedBox(height: sizedBoxHeight),
               Text(
                 username,
-                style: TextStyle(
+                style: const TextStyle(
                   color: Colors.amberAccent,
                   letterSpacing: 2.0,
                   fontSize: 28.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              Text(
+              const Text(
                 "CURRENT NINJA LEVEL",
                 style: TextStyle(
                   color: Colors.grey,
                   letterSpacing: 2.0,
                 ),
               ),
-              SizedBox(height: 10.0),
+              SizedBox(height: sizedBoxHeight),
               Text(
-                "8",
-                style: TextStyle(
+                "$ninjaLevel",
+                style: const TextStyle(
                   color: Colors.amberAccent,
                   letterSpacing: 2.0,
                   fontSize: 28.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 10.0),
+              SizedBox(height: sizedBoxHeight),
               Row(children: <Widget>[
-                Icon(
+                const Icon(
                   Icons.email,
                   color: Colors.grey,
                 ),
-                Text("jezreel.veloso@gmail.com",
-                    style: TextStyle(
+                Text(email,
+                    style: const TextStyle(
                       color: Colors.grey,
                       fontSize: 18.0,
                       letterSpacing: 1.0,
